3b26ac250980f8d2d845db88ce23cbdf49fe18c5
Diana Sosnovskaya <progdidi@gmail.com>
2023-12-21 19:59:19 +0100

    feat: add hamburger menu

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,8 @@
-$(function(){
-   
-});
+// hamburger button
+
+const menuBtn = document.querySelector('.menu__btn');
+const menuList = document.querySelector('.menu__list');
+
+menuBtn.addEventListener('click', () => {
+    menuList.classList.toggle('active');
+})
